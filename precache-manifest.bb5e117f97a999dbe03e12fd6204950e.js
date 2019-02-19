622d578decf2f17cac2be253d09d555e88e921dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-19 20:04:17 +0000

    Deploy SocialGouv/standup to github.com/SocialGouv/standup.git:gh-pages

--- precache-manifest.bb5e117f97a999dbe03e12fd6204950e.js
+++ precache-manifest.bb5e117f97a999dbe03e12fd6204950e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ce64aa7d708398de9913",
+    "revision": "170895f67e17d5711be7",
     "url": "/standup/static/css/main.fcbd2bed.chunk.css"
   },
   {
-    "revision": "ce64aa7d708398de9913",
-    "url": "/standup/static/js/main.ce64aa7d.chunk.js"
+    "revision": "170895f67e17d5711be7",
+    "url": "/standup/static/js/main.170895f6.chunk.js"
   },
   {
     "revision": "a64436a74b5037b0d8de",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/standup/static/js/2.4552ddae.chunk.js"
   },
   {
-    "revision": "d771daab2ab212d80104afe31cdf3706",
+    "revision": "483fce75956a2c9c6f170f077ea5f97e",
     "url": "/standup/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/standup/precache-manifest.fad6a63338a174bd378e52599844a201.js"
+  "/standup/precache-manifest.bb5e117f97a999dbe03e12fd6204950e.js"
 );
 
 workbox.clientsClaim();
