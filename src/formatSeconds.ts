045d9cbf299c258085707537149fa19cd9c682cf
Douglas Duteil <douglasduteil@gmail.com>
2019-03-06 00:34:53 +0100

    fix: 1 minute time bug

--- src/formatSeconds.ts
+++ src/formatSeconds.ts
@@ -5,7 +5,7 @@ const pad = (num: number) => String(num < 10 ? "0" + num : num);
 export const formatSeconds = (seconds: number) => {
   let minutes = 0,
     secs = 0;
-  if (seconds > 60) {
+  if (seconds >= 60) {
     minutes = Math.floor(seconds / 60);
     secs = seconds - minutes * 60;
   } else {
