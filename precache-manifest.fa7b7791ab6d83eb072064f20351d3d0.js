76f166c79b1257f3f5ef9c5b486729fd8d7adc9b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 20:48:35 +0000

    Deploy SocialGouv/standup to github.com/SocialGouv/standup.git:gh-pages

---
+++ precache-manifest.fa7b7791ab6d83eb072064f20351d3d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75ba3ea9788ab53eba61787d9ee6d9a",
+    "url": "/standup/index.html"
+  },
+  {
+    "revision": "d1dc57572bb94ed91ca5",
+    "url": "/standup/static/css/main.be73a2a2.chunk.css"
+  },
+  {
+    "revision": "481552a0fcab5204de18",
+    "url": "/standup/static/js/2.66e470ab.chunk.js"
+  },
+  {
+    "revision": "d1dc57572bb94ed91ca5",
+    "url": "/standup/static/js/main.503b5047.chunk.js"
+  },
+  {
+    "revision": "a64436a74b5037b0d8de",
+    "url": "/standup/static/js/runtime~main.f5bb6ac8.js"
+  }
+]);
