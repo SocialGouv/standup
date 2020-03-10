38155f0429c0bbac0e672c1e1bc0d1dadb9155db
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-10 17:04:28 +0100

    Fix #10

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Post from "./Post"
+import Footer from "./Footer"
 import Default from "./Default"
 import styled from "styled-components"
 
-const Slide = ({ data, team, selected }) => (
+const Slide = ({ data, team, nextTeam, selected }) => (
   <Wrapper
     key={data.team_slug}
     className={`slide${selected ? " selected" : ""}`}
   >
     {team ? <Post data={data} team={team} /> : <Default data={data} />}
+    {nextTeam && <Footer team={nextTeam} />}
   </Wrapper>
 )
 
