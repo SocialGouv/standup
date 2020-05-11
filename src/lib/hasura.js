a8d7f7cce6f44154c0f05874ca105467df498620
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-05-11 18:27:05 +0200

    chore(pipeline): Remove logs.

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -3,7 +3,6 @@ import { GraphQLClient } from "graphql-request"
 const url = process.env.HASURA_URL
 
 export const fetch = async (query, variables) => {
-  console.log("URL:", url)
   const graphQLClient = new GraphQLClient(url, {
     headers: {
       "content-type": "application/json"
