bf3c59526e0927116fac47db89a06379005842ca
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-10 14:47:02 +0200

    fix: Debug Hasura connection.

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -1,6 +1,7 @@
 import { GraphQLClient } from "graphql-request"
 
 const url = process.env.HASURA_URL
+console.log("HASURA_URL", process.env.HASURA_URL)
 
 export const fetch = async (query, variables) => {
   const graphQLClient = new GraphQLClient(url, {
