43660f7a1dbe68f2e5395e7bd3ee1b1e994ae9cb
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-12 03:00:06 +0200

    fix: Pretty KPIs display.

--- src/components/Post/Description.js
+++ src/components/Post/Description.js
@@ -1,7 +1,6 @@
 import React from "react"
 
-const Description = ({ text }) => (
-  <div className="description">&ldquo; {text} &rdquo;</div>
-)
+const Description = ({ text }) =>
+  text && <div className="description">&ldquo; {text} &rdquo;</div>
 
 export default Description
