4f25181eeff275ad353c2bb637724535b2a93814
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-17 01:06:36 +0200

    chore(sentry): Add Sentry. (#283)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
-module.exports = {
-  webpack: function(config) {
+const webpack = require("webpack")
+const nextSourceMaps = require("@zeit/next-source-maps")
+
+module.exports = nextSourceMaps({
+  env: {
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    MATOMO_SITE_ID: process.env.MATOMO_SITE_ID,
+    MATOMO_URL: process.env.MATOMO_URL
+  },
+  webpack: (config, { isServer, buildId }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        // looks like it doesnt work for some reason
+        "process.env.SENTRY_RELEASE": JSON.stringify(buildId)
+      })
+    )
+
     config.module.rules.push({
       test: /\.ya?ml$/,
       use: "js-yaml-loader"
     })
+
+    if (!isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser"
+    }
+
     return config
   }
-}
+})
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import "github-markdown-css"
 import "../styles/main.scss"
+import * as Sentry from "@sentry/node"
 
 import React from "react"
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN
+})
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
