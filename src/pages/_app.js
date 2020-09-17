ce32f23052fe326e80a04055367a3cfd9660a108
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-17 23:09:42 +0200

    fix: Add matomo config.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
 import "github-markdown-css"
 import "../styles/main.scss"
 
+import { initMatomo } from "@lib/matomo"
 import * as Sentry from "@sentry/node"
-import React from "react"
+import React, { useEffect } from "react"
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -10,5 +11,11 @@ Sentry.init({
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    initMatomo({
+      piwikUrl: process.env.MATOMO_URL,
+      siteId: process.env.MATOMO_SITE_ID,
+    })
+  })
   return <Component {...pageProps} />
 }
