2268b72d40d5a163ff42c1a0aad73da63f94e048
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-02-04 14:59:37 +0100

    feat: Add obselete stamp.

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,26 +1,50 @@
 import Extra from "@components/Extra"
 import Missing from "@components/Missing"
 import Post from "@components/Post"
+import Stamp from "@components/Stamp"
 import Weather from "@components/Weather"
+import { addDays, differenceInWeeks, getISODay } from "date-fns"
 import React from "react"
 
-const Slide = ({ id, data }) => (
-  <div id={id} className="slide" key={data.team_slug}>
-    <h1 className="title">
-      <Weather status={data.mood} />
-      {data.team?.name || data.title}
-    </h1>
+const getClosestDayOfLastWeek = (dayOfWeek, fromDate = new Date()) => {
+  const dayOfWeekMap = {
+    Fri: 5,
+    Mon: 1,
+    Sat: 6,
+    Sun: 7,
+    Thur: 4,
+    Tue: 2,
+    Wed: 3,
+  }
+  const offsetDays = -7 - (getISODay(fromDate) - dayOfWeekMap[dayOfWeek])
+  return addDays(fromDate, offsetDays)
+}
 
-    <div className="content">
-      {data.team ? (
-        <Post data={data} />
-      ) : data.teams ? (
-        <Missing teams={data.teams} />
-      ) : (
-        <Extra data={data} />
-      )}
+const isObselete = (date) => {
+  const lastTuesday = getClosestDayOfLastWeek("Tue")
+  console.log("lastTuesday", lastTuesday)
+  return differenceInWeeks(lastTuesday, new Date(date)) >= 2
+}
+
+const Slide = ({ id, data }) =>
+  console.log("data", data) || (
+    <div id={id} className="slide" key={data.team_slug}>
+      <h1 className="title">
+        <Weather status={data.mood} />
+        {data.team?.name || data.title}
+      </h1>
+
+      <div className="content">
+        {isObselete(data.created_at) && <Stamp />}
+        {data.team ? (
+          <Post data={data} />
+        ) : data.teams ? (
+          <Missing teams={data.teams} />
+        ) : (
+          <Extra data={data} />
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
 
 export default Slide
---
+++ src/components/Stamp.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Stamp = () => <span className="stamp">OBSOLETE</span>
+
+export default Stamp
