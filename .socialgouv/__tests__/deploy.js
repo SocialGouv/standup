ed24c2e252bd6f79677040041b2abeffc811d5f3
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-06-09 16:07:52 +0200

    fix: Upgrade k8s version.

--- .socialgouv/__tests__/deploy.js
+++ .socialgouv/__tests__/deploy.js
@@ -9,13 +9,13 @@ jest.setTimeout(1000 * 60)
 test("deploy dev", async () => {
   Object.assign(process.env, project("standup").dev)
   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-  const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.19 --env dev", { env: process.env })
+  const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env dev", { env: process.env })
   expect(manifest).toMatchSnapshot()
 })
 
 test("deploy preprod", async () => {
   Object.assign(process.env, project("standup").preprod)
   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-  const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.19 --env preprod", { env: process.env })
+  const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env preprod", { env: process.env })
   expect(manifest).toMatchSnapshot()
 })
