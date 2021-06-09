11dce0ad1c8ea0450fa12e1fd9a9fe078a01074a
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-06-09 19:41:50 +0200

    fix: Go prod.

--- .socialgouv/__tests__/deploy.js
+++ .socialgouv/__tests__/deploy.js
@@ -19,3 +19,10 @@ test("deploy preprod", async () => {
   const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env preprod", { env: process.env })
   expect(manifest).toMatchSnapshot()
 })
+
+test("deploy prod", async () => {
+  Object.assign(process.env, project("standup").prod)
+  process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
+  const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env prod", { env: process.env })
+  expect(manifest).toMatchSnapshot()
+})
