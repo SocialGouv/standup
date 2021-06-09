ea40482e4edafcdb5c454d8327cd281911224d8d
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-06-09 08:47:22 +0200

    fix: Use preprod suffix for Github env.

--- .socialgouv/__tests__/deploy.js
+++ .socialgouv/__tests__/deploy.js
@@ -9,6 +9,6 @@ jest.setTimeout(1000 * 60)
 test("deploy", async () => {
   Object.assign(process.env, project("standup").preprod)
   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-  const { stdout: manifest} = await exec("npx @socialgouv/k8s@1.9.0-alpha.13", { env: process.env })
+  const { stdout: manifest} = await exec("npx @socialgouv/k8s@1.9.0-alpha.15", { env: process.env })
   expect(manifest).toMatchSnapshot()
 })
