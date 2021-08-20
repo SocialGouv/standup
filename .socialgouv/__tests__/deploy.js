f9e9e54273383cfb589bbd3bd523e1d60d4662bd
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-08-20 11:47:55 +0200

    Update tests.

--- .socialgouv/__tests__/deploy.js
+++ .socialgouv/__tests__/deploy.js
@@ -1,4 +1,3 @@
-import path from "path"
 import util from "util"
 import { directory } from "tempy"
 import { project } from "@socialgouv/kosko-charts/testing/fake/github-actions.env"
@@ -7,40 +6,22 @@ const exec = util.promisify(require('child_process').exec);
 
 jest.setTimeout(1000 * 60)
 
-// test("deploy dev", async () => {
-//   Object.assign(process.env, project("standup").dev)
-//   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-//   const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env dev", { env: process.env })
-//   expect(manifest).toMatchSnapshot()
-// })
-
 test("deploy dev 2", async () => {
   const dir = directory()
   const env = project("standup").dev
-  env.SOCIALGOUV_CONFIG_PATH = `${dir}/nok8s/config.json`
-  console.log("ENV", env);
+
+  env.SOCIALGOUV_CONFIG_PATH = `${dir}/autodevops/config.json`
+
   Object.assign(process.env, env)
-  console.log("SOCIAL:", process.env.SOCIALGOUV_BASE_DOMAIN);
+
   const cmd = `
-    npx degit SocialGouv/kosko-charts/templates/nok8s#101f3ae9478809aa845c5193722b83e8b0bde02a ${dir}/nok8s; \
-    yarn --cwd ${dir}/nok8s --silent; \
-    cp -r ${__dirname}/../environments ${__dirname}/../config.json ${dir}/nok8s/; \
-    yarn --cwd ${dir}/nok8s --silent generate --env dev
+    npx degit SocialGouv/kosko-charts/templates/autodevops ${dir}/autodevops; \
+    yarn --cwd ${dir}/autodevops --silent; \
+    cp -r ${__dirname}/../environments ${__dirname}/../config.json ${dir}/autodevops/; \
+    yarn --cwd ${dir}/autodevops --silent generate --env dev
   `
+
   const { stdout: manifest } = await exec(cmd, { env: process.env })
+
   expect(manifest).toMatchSnapshot()
 })
-
-// test("deploy preprod", async () => {
-//   Object.assign(process.env, project("standup").preprod)
-//   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-//   const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env preprod", { env: process.env })
-//   expect(manifest).toMatchSnapshot()
-// })
-
-// test("deploy prod", async () => {
-//   Object.assign(process.env, project("standup").prod)
-//   process.env.SOCIALGOUV_CONFIG_PATH = path.join(__dirname, "../config.json");
-//   const { stdout: manifest } = await exec("npx @socialgouv/k8s@1.9.0-alpha.20 --env prod", { env: process.env })
-//   expect(manifest).toMatchSnapshot()
-// })
