f9a333d9bf09ade9521aab29b5909b622a8aacdf
devthejo <jo@surikat.pro>
2022-02-14 10:48:54 +0100

    feat(k8s): helm + argocd wip

---
+++ .github/actions/deploy/values.ts
@@ -0,0 +1,80 @@
+import generate from "@socialgouv/env-slug"
+
+const {
+  SOCIALGOUV_PRODUCTION,
+  SOCIALGOUV_PREPRODUCTION,
+  SOCIALGOUV_PRODUCTION_NAMESPACE,
+  SOCIALGOUV_BASE_DOMAIN,
+  GITHUB_REPOSITORY,
+  GITHUB_REF,
+  GITHUB_SHA,
+  KEEP_ALIVE,
+} = process.env;
+
+const isProduction = Boolean(SOCIALGOUV_PRODUCTION);
+const isPreProduction = Boolean(SOCIALGOUV_PREPRODUCTION);
+
+const keepAlive = Boolean(KEEP_ALIVE);
+
+const gitBranch = GITHUB_REF
+
+const branchName = GITHUB_REF.replace("refs/heads/", "").replace(
+  "refs/tags/",
+  ""
+);
+
+const isDev = !isPreProduction && !isProduction;
+const isRenovate = branchName?.startsWith("renovate")
+const isDestroyable = isDev && !keepAlive;
+
+const ttl = isDestroyable ? (isRenovate ? "1d" : "7d") : "";
+
+const projectName = SOCIALGOUV_PRODUCTION_NAMESPACE || GITHUB_REPOSITORY.split("/")[1];
+
+const imageTag = GITHUB_REF.startsWith("refs/tags/")
+  ? (GITHUB_REF.split("/").pop() ?? "").substring(1)
+  : `sha-${GITHUB_SHA}`;
+
+const environmentSlug = generate(branchName);
+
+const productionNamespace = SOCIALGOUV_PRODUCTION_NAMESPACE || projectName;
+const preProductionNamespace = `${projectName}-preprod`;
+const devNamespace = generate(`${projectName}-${branchName}`);
+
+const namespace = isProduction
+  ? productionNamespace
+  : isPreProduction
+  ? preProductionNamespace
+  : devNamespace;
+
+const subdomain = isProduction
+  ? projectName
+  : isPreProduction
+  ? `${projectName}-preprod`
+  : devNamespace;
+
+const MAX_HOSTNAME_SIZE = 53;
+const shortenHost = (hostname: string) => hostname.slice(0, MAX_HOSTNAME_SIZE).replace(/-+$/, "");
+
+const domain = SOCIALGOUV_BASE_DOMAIN
+
+const host = `${shortenHost(subdomain)}.${domain}`;
+
+const registry = `ghcr.io/socialgouv/${projectName}`
+
+const values = {
+  isProduction,
+  ttl,
+  namespace,
+  host,
+  registry,
+  gitBranch,
+  imageTag,
+  // rancherProjectId,
+  // sentryDsn: {
+  //   prod: sentryDsnProd,
+  //   dev: sentryDsnProdDev,
+  // }
+}
+
+console.log(JSON.stringify(values, null, 2))
