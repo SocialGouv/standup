d4f9199055a0c6258063eacd21bfb8d7b9a1074a
devthejo <jo@surikat.pro>
2022-02-15 16:53:16 +0100

    chore(wip): wip

--- .github/actions/deploy/values.ts
+++ .github/actions/deploy/values.ts
@@ -4,22 +4,24 @@ import yaml from "js-yaml"
 const env = process.env
 
 const {
-  SOCIALGOUV_PRODUCTION,
-  SOCIALGOUV_PREPRODUCTION,
-  SOCIALGOUV_PRODUCTION_NAMESPACE,
-  SOCIALGOUV_BASE_DOMAIN,
+  ENVIRONMENT,
+  NAMESPACE,
+  PROJECT_NAME,
+  RANCHER_PROJECT_ID,
+  IMAGE_NAME,
   GITHUB_REPOSITORY,
   GITHUB_REF,
   GITHUB_SHA,
+  BASE_DOMAIN,
   KEEP_ALIVE,
-  RANCHER_PROJECT_ID,
 } = env
 
 const repository = GITHUB_REPOSITORY ?? ""
 const gitBranch = GITHUB_REF ?? ""
 
-const isProduction = Boolean(SOCIALGOUV_PRODUCTION)
-const isPreProduction = Boolean(SOCIALGOUV_PREPRODUCTION)
+const isProduction = ENVIRONMENT === "prod"
+const isPreProduction = ENVIRONMENT === "preprod"
+const isDev = !(isProduction || isPreProduction)
 
 const keepAlive = Boolean(KEEP_ALIVE)
 
@@ -27,45 +29,36 @@ const branchName = gitBranch
   .replace("refs/heads/", "")
   .replace("refs/tags/", "")
 
-const isDev = !isPreProduction && !isProduction
 const isRenovate = branchName.startsWith("renovate")
 const isDestroyable = isDev && !keepAlive
 
 const ttl = isDestroyable ? (isRenovate ? "1d" : "7d") : ""
 
+const imageName = IMAGE_NAME
 const sha = GITHUB_SHA ?? ""
-// const shortSha = sha.slice(0, 7)
 const imageTag = gitBranch.startsWith("refs/tags/")
   ? (gitBranch.split("/").pop() ?? "").substring(1)
   : `sha-${sha}`
 
-const projectName = SOCIALGOUV_PRODUCTION_NAMESPACE || repository.split("/")[1]
+const projectName = PROJECT_NAME
 
-const productionNamespace = SOCIALGOUV_PRODUCTION_NAMESPACE || projectName
-const preProductionNamespace = `${projectName}-preprod`
-const devNamespace = generate(`${projectName}-${branchName}`)
-
-const namespace = isProduction
-  ? productionNamespace
-  : isPreProduction
-  ? preProductionNamespace
-  : devNamespace
+const namespace = NAMESPACE
 
 const subdomain = isProduction
   ? projectName
   : isPreProduction
   ? `${projectName}-preprod`
-  : devNamespace
+  : generate(`${projectName}-${branchName}`)
 
 const MAX_HOSTNAME_SIZE = 53
 const shortenHost = (hostname: string): string =>
   hostname.slice(0, MAX_HOSTNAME_SIZE).replace(/-+$/, "")
 
-const domain = SOCIALGOUV_BASE_DOMAIN ?? ""
+const domain = BASE_DOMAIN ?? ""
 
 const host = `${shortenHost(subdomain)}.${domain}`
 
-const registry = `ghcr.io/socialgouv/${projectName}`
+const registry = `ghcr.io/socialgouv/${imageName ?? projectName}`
 
 const rancherProjectId = RANCHER_PROJECT_ID
 
