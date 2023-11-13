5d5865e37d1df6f853ad16dcca66ce20088c4a36
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-11-13 15:21:30 +0100

    fix: last gif (#463)
    * fix: last gif
    
    * Update sujets-transverses.js

--- src/sujets-transverses.js
+++ src/sujets-transverses.js
@@ -2,15 +2,11 @@
 export default `
 
 
-
-
-### Numérique en communs les 18/19 Octobre
-
-### Forum BETA le 12 Octobre
+# @bientôt
 
 ---
 
-![bon appétit](https://media.giphy.com/media/l0IykUhfRhQkCkzMk/giphy.gif)
+![bon appétit](https://media.giphy.com/media/2s7lb48XP0yje/giphy.gif)
 
 
 `
