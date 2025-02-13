e64af3db95bbe5a21870c56d30585fe5851649d9
Ferréol JEANNOT <ferreol.jeannot@tritons.io>
2025-02-13 11:54:24 +0100

    Update sujets-transverses.js

--- src/sujets-transverses.js
+++ src/sujets-transverses.js
@@ -2,12 +2,6 @@
 export default `
 
 
-- Lundi 20/11 à 13h : présentation du modèle de language Albert (DINUM)
-
-# @bientôt
-
----
-
 ![bon appétit](https://media.giphy.com/media/2s7lb48XP0yje/giphy.gif)
 
 
